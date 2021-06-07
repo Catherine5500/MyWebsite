0dc236bf90dbfa72ba8f889012dccb28c7eb37aa
Catherine Yang <catherineyang5200@gmail.com>
2021-06-07 10:19:43 -0400

    Updates

---
+++ component---src-pages-page-2-js-fc4317df8390989ad5e5.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[617],{9863:function(t,e,l){"use strict";l.r(e),l.d(e,{default:function(){return u}});l(7294);var n=l(5444),r=l(3964);function u(){return(0,r.tZ)("div",null,(0,r.tZ)("h1",null,"Page2"),(0,r.tZ)(n.Link,{to:"/"},"Home "))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-fc4317df8390989ad5e5.js.map
